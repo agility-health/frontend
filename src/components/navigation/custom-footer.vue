345c8fcab9194b2e12752642881cb920a17e51a9
anatoli <Anatoli.Aheichyk@itechart-group.com>
2022-04-14 19:09:26 +0300

    add footer and header

---
+++ src/components/navigation/custom-footer.vue
@@ -0,0 +1,148 @@
+<template>
+    <div class="my-5">
+    <!-- Footer -->
+    <footer
+            class="text-center text-lg-start text-dark"
+            style="background-color: #ECEFF1"
+            >
+        <!-- Section: Social media -->
+        <section
+                class="d-flex justify-content-between p-4 text-white"
+                style="background-color: #54bb44"
+                >
+        <!-- Left -->
+        <div class="me-5">
+            <span>Get connected with us on social networks:</span>
+        </div>
+        <!-- Left -->
+
+        <!-- Right -->
+        <div>
+            <a href="" class="text-white me-4">
+            <i class="fa fa-facebook"></i>
+            </a>
+            <a href="" class="text-white me-4">
+            <i class="fa fa-twitter"></i>
+            </a>
+            <a href="" class="text-white me-4">
+            <i class="fa fa-google"></i>
+            </a>
+            <a href="" class="text-white me-4">
+            <i class="fa fa-instagram"></i>
+            </a>
+            <a href="" class="text-white me-4">
+            <i class="fa fa-linkedin"></i>
+            </a>
+        </div>
+        <!-- Right -->
+        </section>
+        <!-- Section: Social media -->
+
+        <!-- Section: Links  -->
+        <section class="">
+        <div class=" text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                <!-- Content -->
+                <h6 class="text-uppercase fw-bold">AgillityHealth</h6>
+                <hr
+                    class="mb-4 mt-0 d-inline-block mx-auto"
+                    style="width: 60px; background-color: #7c4dff; height: 2px"
+                    />
+                <p>
+                Here you can use rows and columns to organize your footer
+                content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit.
+                </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <!-- Links -->
+                <h6 class="text-uppercase fw-bold">Products</h6>
+                <hr
+                    class="mb-4 mt-0 d-inline-block mx-auto"
+                    style="width: 60px; background-color: #7c4dff; height: 2px"
+                    />
+                <p>
+                <a href="#!" class="text-dark">Clinic</a>
+                </p>
+                <p>
+                <a href="#!" class="text-dark">Our doctors</a>
+                </p>
+                <p>
+                <a href="#!" class="text-dark">Calendar</a>
+                </p>
+                <p>
+                <a href="#!" class="text-dark">Story</a>
+                </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                <!-- Links -->
+                <h6 class="text-uppercase fw-bold">Useful links</h6>
+                <hr
+                    class="mb-4 mt-0 d-inline-block mx-auto"
+                    style="width: 60px; background-color: #7c4dff; height: 2px"
+                    />
+                <p>
+                <a href="/doctor-profile" class="text-dark">Doctor Profile</a>
+                </p>
+                <p>
+                <a href="/patient-profile" class="text-dark">Patient Profile</a>
+                </p>
+                <p>
+                <a href="#!" class="text-dark">Events</a>
+                </p>
+                <p>
+                <a href="#!" class="text-dark">Help</a>
+                </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                <!-- Links -->
+                <h6 class="text-uppercase fw-bold">Contact</h6>
+                <hr
+                    class="mb-4 mt-0 d-inline-block mx-auto"
+                    style="width: 60px; background-color: #7c4dff; height: 2px"
+                    />
+                <p><i class="fas fa-home mr-3"></i> Minsk, 10012, BY</p>
+                <p><i class="fas fa-envelope mr-3"></i> info@eagillityHealt.com</p>
+                <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+                <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+            </div>
+            <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+        </section>
+        <!-- Section: Links  -->
+
+        <!-- Copyright -->
+        <div
+            class="text-center p-3"
+            style="background-color: rgba(0, 0, 0, 0.2)"
+            >
+        © 2022 Copyright:
+        <a class="text-dark" href="/"
+            >agillityHealth.com</a
+            >
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+    </div>
+</template>
+
+<script>
+	export default {
+		name: 'СustomFooter',
+	}
+</script>
